c29301e8de4b3215a99693ede490957e50214528
muzaf <muzafnot@gmail.com>
2026-09-18 13:32:54 +0300

    Superlig v1.6

--- js/modules/superlig.js
+++ js/modules/superlig.js
@@ -28,8 +28,7 @@ const SuperligModule = (() => {
           cache: "no-store",
           signal: controller.signal,
           headers: {
-            "Accept": "application/json, text/plain, */*",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+            "Accept": "application/json, text/plain, */*"
           }
         });
         clearTimeout(timeout);
